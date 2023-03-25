60579eaff274e109c9307f80a4630542329da522
root <root@localhost.localdomain>
2023-03-25 21:31:15 +0100

    making so the leaderboard cant be spammed and making it show 100 fusions

--- js.js
+++ js.js
@@ -29,17 +29,13 @@ function fetch_to_api(endpoint){
 	// Finish by fetching a new fusion
 	new_fusion();
 }
-
-
 async function new_fusion(){
-
     // Fetch a random fusion using API
     fetch('https://justalternate.fr:8080/less_viewed_pokemon')
 		.then((response) => {
 			return response.text();
 		})
 		.then((html) => {
-
 			    // Show the newly fetched fusion
 			    const lines = html.split('\n');
 		 	    // Update current fusion name for future post request
@@ -47,7 +43,6 @@ async function new_fusion(){
     			    document.getElementById("image_fusion").src=("CustomBattlers/"+lines[1]+"/"+lines[0]+"");
     			    document.getElementById("parent1").src=("VanillaBattlers/"+lines[1]+".png");
     			    document.getElementById("parent2").src=("VanillaBattlers/"+lines[2]+".png");
-
 		})
     // Wait a 700 ms before reactivate buttons in order to avoid spam
     await new Promise(resolve => setTimeout(resolve, 700));
@@ -55,3 +50,31 @@ async function new_fusion(){
     document.getElementById("button_refresh").onclick=function(){fetch_to_api('pass');}; 
     document.getElementById("button_pass").onclick=function(){fetch_to_api('refresh');}; 
 } 
+
+async function fetch_leaderboard(){
+    const leaderboard_div = document.getElementById("leaderboard");
+    while (leaderboard_div.hasChildNodes()){
+    	leaderboard_div.removeChild(leaderboard_div.firstChild);
+    }
+    document.getElementById("button_leaderboard").onclick=null; 
+    fetch('https://justalternate.fr:8080/leaderboard')
+		.then((response) => {
+			return response.text();
+		})
+		.then((html) => {
+			console.log(html);
+			const lines = html.split('\n');
+			for (var i=0; i< lines.length -1; i++){
+				let elem = lines[i];
+				let elem_split_point = elem.split('.');
+				let parent1 = elem_split_point[0];
+				let elem_of_leaderboard = document.createElement("img")
+				elem_of_leaderboard.src = ("CustomBattlers/"+parent1+"/"+elem+"");
+				leaderboard_div.appendChild(elem_of_leaderboard);
+
+
+			}
+		})
+    await new Promise(resolve => setTimeout(resolve, 5000));
+    document.getElementById("button_leaderboard").onclick=function(){fetch_leaderboard();}; 
+}
